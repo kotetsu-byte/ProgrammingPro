5f1f8fd3a920720d0ad44b2f37f1b2f30ed016f6
kotetsu-byte <gross.23.29.11@gmail.com>
2024-05-17 10:48:01 +0500

    "5/17/2024"

--- Server/Models/Course.cs
+++ Server/Models/Course.cs
@@ -8,7 +8,6 @@ namespace ProgrammingPro.Server.Models
         public int? Id { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
-        public string? ImgName { get; set; }
         public ICollection<Lesson>? Lessons { get; set; }
         public ICollection<Test> Tests { get; set; }
     }
